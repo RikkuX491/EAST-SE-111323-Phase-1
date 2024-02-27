29945f32624f9dbd44d1e8387c547d34e24c6aaf
kush Muhamad <kushmohammed98@yahoo.com>
2024-02-28 01:24:22 +0300

    my first deliverables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 // write your code here
+
+// Deliverable 1
+const greeting = "Welcome to Chaps"
+
+//Deliverable 2
+let num1 = 4
+let num2 = 6
+
+//deliverable 3
+let SumString = num1 + num2
+console.log(SumString)
+
+
+  
