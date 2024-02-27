a0fa5c83ee9c68b817634c1a1f290b87a3ae9daf
kush Muhamad <kushmohammed98@yahoo.com>
2024-02-28 01:38:55 +0300

    4 deliverables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 // write your code here
+// deliverable 1
+const greeting = "welcome home"
+
+//deliverable 2
+let num1 = 2
+let num2 = 4
+
+//deliverable 3
+
+let sum = num1 + num2
+
+//deliverable 4 
+let sumString = `${num1} + ${num2} = ${sum}`
+console.log(sumString)
